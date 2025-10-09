d4062e53cb6172cf23dac12888dc3f5bb27a5ad9
Hassan Jamil <hassanjamil91@gmail.com>
2025-10-09 19:52:47 +0400

    * improved mapper

--- src/data/mappers/userMapper.ts
+++ src/data/mappers/userMapper.ts
@@ -5,20 +5,10 @@ export const mapUserDtoToUser = (dto: UserDto): User => ({
   id: dto.id,
   name: dto.name,
   username: dto.username,
-  email: dto.email,
-  address: dto.address,
-  phone: dto.phone,
-  website: dto.website,
-  company: dto.company,
 });
 
 export const mapUserToUserDto = (user: User): UserDto => ({
   id: user.id,
   name: user.name,
   username: user.username,
-  email: user.email,
-  address: user.address,
-  phone: user.phone,
-  website: user.website,
-  company: user.company,
 });
